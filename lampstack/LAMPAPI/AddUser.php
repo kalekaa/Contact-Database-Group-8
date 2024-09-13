32fc391e9ba6650ecb126af045ae8f503ef069b5
caitlin <113788299+alaskasalad@users.noreply.github.com>
2024-09-13 10:39:18 -0400

    Update AddUser.php

--- lampstack/LAMPAPI/AddUser.php
+++ lampstack/LAMPAPI/AddUser.php
@@ -2,9 +2,8 @@
 
     $inData = getRequestInfo();
 
-    $id = 0;
-	$firstName = $inData["firstname"];
-	$lastName = $inData["lastname"];
+    $firstName = $inData["firstname"];
+    $lastName = $inData["lastname"];
     $login = $inData["login"];
     $password = $inData["password"];
 
@@ -14,47 +13,35 @@
     {
         returnWithError( $conn->connect_error );
     }
-    else
+   else
     {
         // prepare for all the columns that u will insert into 
-        $stmt = $conn->prepare("INSERT into Users (ID, FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
         
         // bind to the table Users () id firstName lastName login password
-        // "issss" is 4 string types and 1 int 
-        $stmt->bind_param("issss", $id, $firstName, $lastName, $login, $password);
-        $stmt->execute()
-
-        if ($stmt->execute())
-        {
-            if ($stmt->affected_rows > 0)
-                returnWithInfo($firstName, $lastName, $login);
-            else
-            returnWithError("Could not add user");
-        }
-
-        // close the prepared statement 
-        $stmt->close();
-
-        // close the connection 
-        $conn->close();
-		returnWithError("");
+        // "ssss" is 4 string types and 1 int 
+        $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+        $stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
     }
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
 
-    function sendResultInfoAsJson( $obj )
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
-
-    function returnWithError( $err )
-    {
-        $retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson( $retValue );
-    }
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+	
+function returnWithError( $err )
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson( $retValue );
+}
 
 ?>
