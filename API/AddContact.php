14472d062bc01321f290e4ea91160b6c0313717b
caitlin <113788299+alaskasalad@users.noreply.github.com>
2024-09-17 10:23:39 -0400

    Update AddContact.php

--- API/AddContact.php
+++ API/AddContact.php
@@ -2,7 +2,6 @@
 
     $inData = getRequestInfo();
 
-    $id = 0; // Increment from last id
     $name = $inData["name"];
     $phone = $inData["phone"];
     $email = $inData["email"];
@@ -17,26 +16,16 @@
     else
     {
         // prepare for all the columns that u will insert into 
-        $stmt = $conn->prepare("INSERT into Contacts (ID, Name, Phone, Email, UserId) VALUES (?, ?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT into Contacts (Name, Phone, Email, UserId) VALUES (?, ?, ?, ?)");
         
         // bind to the table contacts () id name phone email userId
-        // "isssi" is 3 string types and 2 ints
-        $stmt->bind_param("isssi", $id, $name, $phone, $email, $userId);
+        // "sssi" is 3 string types and 1 ints
+        $stmt->bind_param("sssi", $name, $phone, $email, $userId);
 
-        if ($stmt->execute())
-        {
-            if ($stmt->affected_rows > 0)
-                returnWithInfo($name, $phone, $email);
-            else
-                returnWithError("Could not add contact");
-        }
-
-        // close the prepared statement 
+        $stmt->execute();
         $stmt->close();
-
-        // close the connection 
         $conn->close();
-		returnWithError("");
+        returnWithError("");
     }
 
     function getRequestInfo()
